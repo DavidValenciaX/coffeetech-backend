8bb34698627da3b9fa04e57ac0e391aaf6f137d2
DavidValencia526 <davidvalencia0526@gmail.com>
2025-04-25 23:28:39 -0500

    Refactor farm and plot models: Reorganize and restore classes for Farms, FarmStates, Plots, and related relationships in models.py for improved structure and clarity.

--- models/models.py
+++ models/models.py
@@ -5,60 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class FarmStates(Base):
-    __tablename__ = 'farm_states'
-    farm_state_id = Column(Integer, primary_key=True)
-    name = Column(String(45), nullable=False, unique=True)
-    farms = relationship("Farms", back_populates="state")
-
-class Farms(Base):
-    __tablename__ = 'farms'
-
-    farm_id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
-    area = Column(Numeric(10, 2), nullable=False)
-    area_unit_id = Column(Integer, ForeignKey('area_units.area_unit_id'), nullable=False)
-    farm_state_id = Column(Integer, ForeignKey('farm_states.farm_state_id'), nullable=False)
-    __table_args__ = (CheckConstraint('area > 0', name='check_area_positive'),)
-
-    # Relaciones
-    area_unit = relationship("AreaUnits")
-    state = relationship("FarmStates")
-    invitations = relationship("Invitations", back_populates="farm")
-    user_roles_farms = relationship('UserRoleFarm', back_populates='farm')
-    plots = relationship("Plots", back_populates="farm")
-    notifications = relationship("Notifications", back_populates="farm")
-
-class UserRoleFarmStates(Base):
-    __tablename__ = 'user_role_farm_states'
-    user_role_farm_state_id = Column(Integer, primary_key=True)
-    name = Column(String(45), nullable=False, unique=True)
-    user_role_farm = relationship("UserRoleFarm", back_populates="state")
-
-class UserRoleFarm(Base):
-    __tablename__ = 'user_role_farm'
-
-    user_role_farm_id = Column(Integer, primary_key=True)
-    role_id = Column(Integer, ForeignKey('roles.role_id'), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
-    farm_id = Column(Integer, ForeignKey('farms.farm_id'), nullable=False)
-    user_role_farm_state_id = Column(Integer, ForeignKey('user_role_farm_states.user_role_farm_state_id'), nullable=False)
-    __table_args__ = (UniqueConstraint('user_id', 'role_id', 'farm_id'),)
-
-    # Relaciones
-    user = relationship('Users', back_populates='user_roles_farms')
-    farm = relationship('Farms', back_populates='user_roles_farms')
-    role = relationship('Roles', back_populates='user_roles_farms')
-    state = relationship('UserRoleFarmStates', back_populates='user_role_farm')
-
-class AreaUnits(Base):
-    __tablename__ = 'area_units'
-
-    area_unit_id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False, unique=True)
-    abbreviation = Column(String(10), nullable=False, unique=True)
-    farms = relationship("Farms", back_populates="area_unit")
-    plots = relationship("Plots", back_populates="area_unit")
+# Users
 
 class UserStates(Base):
     __tablename__ = 'user_states'
@@ -84,7 +31,7 @@ class Users(Base):
     notifications = relationship("Notifications", back_populates="user")
     created_transactions = relationship("Transactions", back_populates="creator")
     created_invitations = relationship("Invitations", foreign_keys="[Invitations.inviter_user_id]", back_populates="inviter")
-
+    
 class Roles(Base):
     __tablename__ = 'roles'
 
@@ -115,6 +62,86 @@ class RolePermission(Base):
     # Relaciones con Roles y Permissions
     role = relationship("Roles", back_populates="permissions")
     permission = relationship("Permissions", back_populates="roles")
+    
+# Farms
+
+class FarmStates(Base):
+    __tablename__ = 'farm_states'
+    farm_state_id = Column(Integer, primary_key=True)
+    name = Column(String(45), nullable=False, unique=True)
+    farms = relationship("Farms", back_populates="state")
+
+class Farms(Base):
+    __tablename__ = 'farms'
+
+    farm_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    area = Column(Numeric(10, 2), nullable=False)
+    area_unit_id = Column(Integer, ForeignKey('area_units.area_unit_id'), nullable=False)
+    farm_state_id = Column(Integer, ForeignKey('farm_states.farm_state_id'), nullable=False)
+    __table_args__ = (CheckConstraint('area > 0', name='check_area_positive'),)
+
+    # Relaciones
+    area_unit = relationship("AreaUnits")
+    state = relationship("FarmStates")
+    invitations = relationship("Invitations", back_populates="farm")
+    user_roles_farms = relationship('UserRoleFarm', back_populates='farm')
+    plots = relationship("Plots", back_populates="farm")
+    notifications = relationship("Notifications", back_populates="farm")
+    
+class PlotStates(Base):
+    __tablename__ = 'plot_states'
+    plot_state_id = Column(Integer, primary_key=True)
+    name = Column(String(45), nullable=False, unique=True)
+    plots = relationship("Plots", back_populates="state")
+
+class Plots(Base):
+    __tablename__ = 'plots'
+    __table_args__ = (
+        UniqueConstraint('name', 'farm_id'),
+        CheckConstraint('area > 0'),
+        CheckConstraint('longitude BETWEEN -180 AND 180'),
+        CheckConstraint('latitude BETWEEN -90 AND 90'),
+        CheckConstraint('altitude >= 0 AND altitude <= 3000'),
+    )
+
+    plot_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    longitude = Column(Numeric(11, 8), nullable=True)
+    latitude = Column(Numeric(11, 8), nullable=True)
+    altitude = Column(Numeric(10, 2), nullable=True)
+    coffee_variety_id = Column(Integer, ForeignKey('coffee_varieties.coffee_variety_id'), nullable=False)
+    farm_id = Column(Integer, ForeignKey('farms.farm_id'), nullable=False)
+    area = Column(Numeric(10, 2), nullable=False)
+    area_unit_id = Column(Integer, ForeignKey('area_units.area_unit_id'), nullable=False)
+    plot_state_id = Column(Integer, ForeignKey('plot_states.plot_state_id'), nullable=False)
+
+    # Relaciones
+    farm = relationship("Farms", back_populates="plots")
+    coffee_variety = relationship("CoffeeVarieties", back_populates="plots")
+    state = relationship("PlotStates", back_populates="plots")
+    area_unit = relationship("AreaUnits", back_populates="plots")
+    transactions = relationship("Transactions", back_populates="plot")
+    
+class CoffeeVarieties(Base):
+    __tablename__ = 'coffee_varieties'
+
+    coffee_variety_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+
+    # Relaciones
+    plots = relationship("Plots", back_populates="coffee_variety")
+    
+class AreaUnits(Base):
+    __tablename__ = 'area_units'
+
+    area_unit_id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    abbreviation = Column(String(10), nullable=False, unique=True)
+    farms = relationship("Farms", back_populates="area_unit")
+    plots = relationship("Plots", back_populates="area_unit")
+    
+# Invitations
 
 class InvitationStates(Base):
     __tablename__ = 'invitation_states'
@@ -140,6 +167,8 @@ class Invitations(Base):
     inviter = relationship("Users", foreign_keys=[inviter_user_id], back_populates="created_invitations")
     notifications = relationship("Notifications", back_populates="invitation")
     suggested_role = relationship('Roles', back_populates="invitations")
+    
+# Notifications
 
 class NotificationStates(Base):
     __tablename__ = 'notification_states'
@@ -174,55 +203,23 @@ class Notifications(Base):
     farm = relationship("Farms", back_populates="notifications")
     notification_type = relationship("NotificationTypes", back_populates="notifications")
     state = relationship("NotificationStates", back_populates="notifications")
-
-class CoffeeVarieties(Base):
-    __tablename__ = 'coffee_varieties'
-
-    coffee_variety_id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False, unique=True)
-
-    # Relaciones
-    plots = relationship("Plots", back_populates="coffee_variety")
-
-class PlotStates(Base):
-    __tablename__ = 'plot_states'
-    plot_state_id = Column(Integer, primary_key=True)
-    name = Column(String(45), nullable=False, unique=True)
-    plots = relationship("Plots", back_populates="state")
-
-class Plots(Base):
-    __tablename__ = 'plots'
-    __table_args__ = (
-        UniqueConstraint('name', 'farm_id'),
-        CheckConstraint('area > 0'),
-        CheckConstraint('longitude BETWEEN -180 AND 180'),
-        CheckConstraint('latitude BETWEEN -90 AND 90'),
-        CheckConstraint('altitude >= 0 AND altitude <= 3000'),
-    )
-
-    plot_id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
-    longitude = Column(Numeric(11, 8), nullable=True)
-    latitude = Column(Numeric(11, 8), nullable=True)
-    altitude = Column(Numeric(10, 2), nullable=True)
-    coffee_variety_id = Column(Integer, ForeignKey('coffee_varieties.coffee_variety_id'), nullable=False)
-    farm_id = Column(Integer, ForeignKey('farms.farm_id'), nullable=False)
-    area = Column(Numeric(10, 2), nullable=False)
-    area_unit_id = Column(Integer, ForeignKey('area_units.area_unit_id'), nullable=False)
-    plot_state_id = Column(Integer, ForeignKey('plot_states.plot_state_id'), nullable=False)
-
-    # Relaciones
-    farm = relationship("Farms", back_populates="plots")
-    coffee_variety = relationship("CoffeeVarieties", back_populates="plots")
-    state = relationship("PlotStates", back_populates="plots")
-    area_unit = relationship("AreaUnits", back_populates="plots")
-    transactions = relationship("Transactions", back_populates="plot")
+    
+# Transactions
 
 class TransactionStates(Base):
     __tablename__ = 'transaction_states'
     transaction_state_id = Column(Integer, primary_key=True)
     name = Column(String(45), nullable=False, unique=True)
     transactions = relationship("Transactions", back_populates="state")
+    
+class TransactionTypes(Base):
+    __tablename__ = 'transaction_types'
+
+    transaction_type_id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+
+    # Relaciones
+    categories = relationship("TransactionCategories", back_populates="transaction_type")
 
 class TransactionCategories(Base):
     __tablename__ = 'transaction_categories'
@@ -235,15 +232,6 @@ class TransactionCategories(Base):
     # Relaciones
     transaction_type = relationship("TransactionTypes", back_populates="categories")
     transactions = relationship("Transactions", back_populates="transaction_category")
-
-class TransactionTypes(Base):
-    __tablename__ = 'transaction_types'
-
-    transaction_type_id = Column(Integer, primary_key=True)
-    name = Column(String(255), nullable=False, unique=True)
-
-    # Relaciones
-    categories = relationship("TransactionCategories", back_populates="transaction_type")
 
 class Transactions(Base):
     __tablename__ = 'transactions'
@@ -262,3 +250,27 @@ class Transactions(Base):
     transaction_category = relationship("TransactionCategories", back_populates="transactions")
     state = relationship("TransactionStates", back_populates="transactions")
     creator = relationship("Users", back_populates="created_transactions")
+    
+# UserRoleFarm
+    
+class UserRoleFarmStates(Base):
+    __tablename__ = 'user_role_farm_states'
+    user_role_farm_state_id = Column(Integer, primary_key=True)
+    name = Column(String(45), nullable=False, unique=True)
+    user_role_farm = relationship("UserRoleFarm", back_populates="state")
+
+class UserRoleFarm(Base):
+    __tablename__ = 'user_role_farm'
+
+    user_role_farm_id = Column(Integer, primary_key=True)
+    role_id = Column(Integer, ForeignKey('roles.role_id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
+    farm_id = Column(Integer, ForeignKey('farms.farm_id'), nullable=False)
+    user_role_farm_state_id = Column(Integer, ForeignKey('user_role_farm_states.user_role_farm_state_id'), nullable=False)
+    __table_args__ = (UniqueConstraint('user_id', 'role_id', 'farm_id'),)
+
+    # Relaciones
+    user = relationship('Users', back_populates='user_roles_farms')
+    farm = relationship('Farms', back_populates='user_roles_farms')
+    role = relationship('Roles', back_populates='user_roles_farms')
+    state = relationship('UserRoleFarmStates', back_populates='user_role_farm')
